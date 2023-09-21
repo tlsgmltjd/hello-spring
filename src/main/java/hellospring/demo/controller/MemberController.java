e6266a3414ade7c62d22727419e205489cd91b34
신희성 <vvwv3092@gmail.com>
2023-09-21 23:36:44 +0900

    Add MVC get user

--- src/main/java/hellospring/demo/controller/MemberController.java
+++ src/main/java/hellospring/demo/controller/MemberController.java
@@ -4,8 +4,11 @@ import hellospring.demo.domain.Member;
 import hellospring.demo.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -26,5 +29,12 @@ public class MemberController {
         member.setName(form.getName());
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
