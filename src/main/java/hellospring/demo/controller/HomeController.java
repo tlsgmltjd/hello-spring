8616d6afd994477ef65d4c7b3a8bf4a7e14299c7
신희성 <vvwv3092@gmail.com>
2023-09-21 23:30:00 +0900

    Add MVC Signup Page

--- src/main/java/hellospring/demo/controller/HomeController.java
+++ src/main/java/hellospring/demo/controller/HomeController.java
@@ -1,7 +1,9 @@
 package hellospring.demo.controller;
 
+import hellospring.demo.domain.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
