391a6a6295e7d903c2bc7ec9f4deaa711f21408a
신희성 <vvwv3092@gmail.com>
2023-10-25 23:04:40 +0900

    Add SpringDataJpa

--- src/main/java/hellospring/demo/SpringConfig.java
+++ src/main/java/hellospring/demo/SpringConfig.java
@@ -16,28 +16,34 @@ public class SpringConfig {
 
 //    private final DataSource dataSource;
 
-    @PersistenceContext
-    private final EntityManager em;
+//    @PersistenceContext
+//    private final EntityManager em;
 
 //    public SpringConfig(DataSource dataSource) {
 //        this.dataSource = dataSource;
 //    }
 
 
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
+
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
 }
---
+++ src/main/java/hellospring/demo/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,11 @@
+package hellospring.demo.repository;
+
+import hellospring.demo.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+    @Override
+    Optional<Member> findByName(String name);
+}
